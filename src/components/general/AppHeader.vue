ffa54a7a0686f2f845d79d52e4e0cf29f6d08b98
Luka <luka.tsaguria@icloud.com>
2023-02-08 14:56:14 +0200

    004 - Add App Header

---
+++ src/components/general/AppHeader.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .app-header
+    .header-container
+      h2 Phone Book
+</template>
+<style scoped lang="sass">
+.app-header
+  width: 100%
+  height: 80px
+  background: purple
+  display: flex
+  align-items: center
+  .header-container
+    width: 95%
+    margin: auto
+    h2
+      color: white
+      font-size: calc(24px + 10 * ((100vw - 320px) / (1440 - 320)))
+</style>
