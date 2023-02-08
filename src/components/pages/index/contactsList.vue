0620743a9f893d1fb34fce7b91d3c30b52a4a90d
Luka <luka.tsaguria@icloud.com>
2023-02-08 16:43:17 +0200

    006 - Added Contact Card | Refactored Contacts List

---
+++ src/components/pages/index/contactsList.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .contacts-list
+    PagesIndexSubComponentsContactCard(v-for="i in 15")
+</template>
+<style scoped lang="sass">
+.contacts-list
+  display: grid
+  grid-template-columns: repeat(4, 1fr)
+  gap: 1rem
+  background-color: #c4c4c4
+  height: auto
+  margin: 30px
+  padding: 30px
+  border-radius: 30px
+</style>
