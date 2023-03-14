9d7d55949532cd7ba0eda1a4ab72d4841129ce99
Luka <luka@unpwr.com>
2023-03-14 10:55:42 +0200

    test

---
+++ src/components/pages/index/SubComponents/ContactsPagination.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+.contacts-pagination
+  button(v-for="page in pagination_pages" :class="active_button === page - 1 ? 'active' : ''" @click="chose_pagination_page(page - 1)") {{ page }}
+</template>
+<script>
+export default {
+  data: () => ({
+    active_button: 0
+  }),
+  watch: {
+    pagination_pages() {
+      const currentPaginationPage = this.pagination_pages - 1
+      const localStoragePaginationPage = localStorage.getItem('selected_pagination_page')
+      if ( currentPaginationPage < localStoragePaginationPage) {
+        const shortageCompensation = localStoragePaginationPage - currentPaginationPage
+        this.chose_pagination_page(localStoragePaginationPage - shortageCompensation)
+      }
+    }
+  },
+  computed: {
+    pagination_pages() {
+      return this.$store.getters.get_pagination_pages
+    },
+  },
+  methods: {
+    chose_pagination_page(i) {
+      this.active_button = i
+      localStorage.setItem('selected_pagination_page', i)
+      this.$store.dispatch('selectPaginationPage')
+    }
+  }
+}
+</script>
+<style scoped lang="sass">
+.contacts-pagination
+  display: flex
+  align-items: center
+  justify-content: space-between
+  max-width: 500px
+  height: 50px
+  background-color: white
+  margin-bottom: 20px
+  border-radius: 30px
+  padding-left: 15px
+  padding-right: 15px
+  button
+    width: 40px
+    height: 24px
+    margin-right: 1px
+    margin-left: 1px
+  .active
+    background-color: rgba(128, 0, 128, 0.85)
+    color: white
+</style>
--- src/components/pages/index/contactsList.vue
+++ src/components/pages/index/contactsList.vue
@@ -2,15 +2,39 @@
   .contacts-list
     .list-header
       PagesIndexSubComponentsContactsMenu
+      PagesIndexSubComponentsContactsPagination
       PagesIndexSubComponentsCreateContact
     .contact-cards
-      PagesIndexSubComponentsContactCard(v-for="contact in contacts" :key="contact.id" :contact_data="contact")
+      PagesIndexSubComponentsContactCard(v-for="contact in contacts[pagination_page]" :key="contact.id" :contact_data="contact")
 </template>
 <script>
 export default {
+  data: () => ({
+    contacts: [],
+    selected_pagination_page: localStorage.getItem('selected_pagination_page')
+  }),
+  watch: {
+    get_contacts() {
+      this.paginate()
+    }
+  },
   computed: {
-    contacts() {
+    get_contacts() {
       return this.$store.getters.get_contacts
+    },
+    pagination_page() {
+      return this.$store.getters.get_selected_pagination_page
+    }
+  },
+  methods: {
+    paginate() {
+      this.contacts = []
+      const contacts = [...this.get_contacts]
+      for (let i = 0; i < contacts.length; i += 12) {
+        const chunk = contacts.slice(i, i + 12);
+        this.contacts.push(chunk);
+      }
+      this.$store.dispatch('setPaginationPages', this.contacts.length)
     }
   }
 }
